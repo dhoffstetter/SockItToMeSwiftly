8a1ab5599699991a3503befb46dedabe95328a60
Diane Hoffstetter <dhoffstetter@mac.com>
2019-12-29 15:55:46 -0800

    Initial Commit

---
+++ SockItToMeSwiftly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SockItToMeSwiftly
+//
+//  Created by Diane Hoffstetter on 12/29/19.
+//  Copyright © 2019 Dumb Blonde Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
